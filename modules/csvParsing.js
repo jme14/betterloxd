0def04fa44d26719864ee534aba621b0e6bd4e7f
jme14 <williamsltrey3@gmail.com>
2024-01-03 17:42:58 -0600

    finished yearEndListMaker draft

--- modules/csvParsing.js
+++ modules/csvParsing.js
@@ -1,23 +1,65 @@
 
+function splitRecordWithQuotes(record){
+    const getTitle = record.split("\"")
+    let tempData = [] 
+
+    const beforeTitleSplit = getTitle[0].split(",")
+    let afterTitleSplit = getTitle[2].split(",")
+
+    for ( let j = 0 ; j < beforeTitleSplit.length-1 ; j++ ){
+        tempData.push(beforeTitleSplit[j])
+    }
+    tempData.push(getTitle[1])
+    for ( let j = 1 ; j < afterTitleSplit.length ; j++ ){
+        tempData.push(afterTitleSplit[j])
+    }
+
+    return tempData
+}
+
+export function readLetterboxdDataDiary(data){
+    data = data.split("\n")
+
+    for ( let i = 1 ; i < data.length ; i++){
+        if ( /"/.test(data[i])){
+            const insertionContent = splitRecordWithQuotes(data[i])
+            if(insertionContent.length < 8) continue
+            data[i] = insertionContent
+        } else {
+            const insertionContent = data[i].split(",")
+            if(insertionContent.length < 8) continue
+            data[i] = insertionContent
+        }
+
+        data[i][7] = data[i][7].split("\r")[0]
+    }
+    data = data.slice(1)
+    while(data[data.length-1].length !== 8){
+        data.pop()
+    }
+
+    let realData = []
+
+    for ( let i = 0 ; i < data.length ; i++ ){
+        let newRecord = [];
+
+        newRecord.push(i+1) // position 
+        newRecord.push(data[i][1]) // title  
+        newRecord.push(data[i][2]) // year 
+        newRecord.push(data[i][3]) // url 
+        newRecord.push(data[i][4]) // rating 
+        newRecord.push(data[i][data[i].length-1]) // watchedDate
+
+        realData.push(newRecord)
+    }
+    return realData
+}
 export function readLetterboxdDataRatings(data){
     data = data.split("\n")
 
     for ( let i = 1 ; i < data.length ; i++ ){
         if ( /"/.test(data[i])){ //if quotation marks exist, treat differently
-            const getTitle = data[i].split("\"")
-            let tempData = [] 
-
-            const beforeTitleSplit = getTitle[0].split(",")
-            let afterTitleSplit = getTitle[2].split(",")
-
-            for ( let j = 0 ; j < beforeTitleSplit.length-1 ; j++ ){
-                tempData.push(beforeTitleSplit[j])
-            }
-            tempData.push(getTitle[1])
-            for ( let j = 1 ; j < afterTitleSplit.length ; j++ ){
-                tempData.push(afterTitleSplit[j])
-            }
-            data[i] = tempData
+            data[i] = splitRecordWithQuotes(data[i])
         }
         else{
             const insertionContent = data[i].split(",")
---
+++ scripts/yearWatchedListMaker.js
@@ -0,0 +1,49 @@
+import ready from "../modules/ready.js"
+import {readLetterboxdDataDiary} from "../modules/csvParsing.js"
+
+import writeListForDownload from "../modules/listWriting.js"
+
+function readDiaryFile(){
+    const input = document.getElementById("diaryCsv")
+
+    const file = input.files[0]
+
+    if ( file ){
+        const reader = new FileReader()
+        reader.onload = function(e){
+            const content = e.target.result
+            useReadData(readLetterboxdDataDiary(content))
+        }
+
+        reader.readAsText(file)
+    } else {
+        alert("Please select a file")
+    }
+}
+
+function main(){
+    const submitButton = document.getElementById("diaryCsvSubmit")
+    submitButton.addEventListener("click", readDiaryFile)
+
+}
+
+function useReadData(data){
+
+    const yearSubmitted = document.getElementById("yearPicker").value
+
+    function isInRange(record){
+        const year = record[5].split("-")[0]
+        return year === yearSubmitted
+    }
+
+    data = data.filter(isInRange)
+
+    for ( let i = 0 ; i < data.length; i++ ){
+        console.log(data[i])
+    }
+    
+    writeListForDownload(data)
+}
+
+ready(main)
+
